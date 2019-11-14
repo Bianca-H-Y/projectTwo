bc3e58e5b732503c3b958a16c6a746a2b9c668a1
Bianca-why <56407953+Bianca-why@users.noreply.github.com>
2019-11-13 23:37:12 -0700

    Deleted all pre-exisitng code comments

--- list_pagination_and_filtering-v1 (2)/js/script.js
+++ list_pagination_and_filtering-v1 (2)/js/script.js
@@ -5,19 +5,8 @@ FSJS project 2 - List Filter and Pagination
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-//Create global variables that reference the student list and will be used for functions//
-//Targets the UL class element Student list
+//Create global variables that referencse the student list and will be used for functions//
+//Target the UL class element Student list
 const studentList = document.querySelectorAll('.student-item');
 //correctly targeting the student-items
 //Now I want to create a variable that target for all children of the class 'student-list' 
@@ -25,22 +14,6 @@ const studentList = document.querySelectorAll('.student-item');
 const tenStudents = 10;
 //Dev Tools don't seem to pinpoint any issues with code above. Moving to creating a function.
 
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 //Function that shows 10 students a page with 2 parameters passed in.
 function showTenPage(list, page) {
     let startIndex = (page * tenStudents) - tenStudents; //Display a "page" gives math//
@@ -60,14 +33,11 @@ function showTenPage(list, page) {
         //wasn't getting errors but reallized test file wasn't attached to HTML--testing now
     }
 }
-}//error where coming from missing closing curly brace.
+}//error wqas coming from missing closing curly brace.
 
 
-//Function for navigation buttons
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
+/*** Function for navigation buttons: Creating an `appendPageLinks function` which
+ * will create the pagination buttons.
 ***/
 //use the appendPageLinks FUnction in the study guide--it builds the code
 //divide total # list items by max number of items per page
@@ -117,8 +87,3 @@ function appendPageLinks(list) {
 //call the functions
 showTenPage(studentList, 1);
 appendPageLinks(studentList);
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
